99f263d670d4ffb61c522570e38c9d2f7bbfea8d
shani-shil100x <shanishil100x@gmail.com>
2025-09-17 13:44:40 +0530

    update code for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,13 +10,13 @@ const App = () => {
   const { authUser } = useContext(AuthContext);
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain ">
+    <div className="bg-[url('/bgImage.svg')] bg-contain ">
       <Toaster />
       <Routes>
         <Route
           path="/"
           element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        /> 
+        />
         <Route
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to="/" />}
--- server/server.js
+++ server/server.js
@@ -49,6 +49,11 @@ app.use("/api/messages", messageRouter);
 // Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-// Server start
-server.listen(PORT, () => console.log("Server running on PORT: " + PORT));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  // Server start
+  server.listen(PORT, () => console.log("Server running on PORT: " + PORT));
+}
+
+//  Export server for Vercel
+export default server;
